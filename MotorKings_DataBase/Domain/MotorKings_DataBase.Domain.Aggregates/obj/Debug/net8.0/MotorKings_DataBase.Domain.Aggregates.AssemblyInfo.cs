887ec123127c8a493bcb1bd8d0336e4528da4e71
NowName2077 <nowname2077@gmail.com>
2025-11-14 04:29:32 +0700

    Update 1.1

--- MotorKings_DataBase/Domain/MotorKings_DataBase.Domain.Aggregates/obj/Debug/net8.0/MotorKings_DataBase.Domain.Aggregates.AssemblyInfo.cs
+++ MotorKings_DataBase/Domain/MotorKings_DataBase.Domain.Aggregates/obj/Debug/net8.0/MotorKings_DataBase.Domain.Aggregates.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MotorKings_DataBase.Domain.Aggregates")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72bbad82e4e9420dcfdabf08f4ee6f93668ed0a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85c0ac040f1c654e3417da7add835383af5390f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MotorKings_DataBase.Domain.Aggregates")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MotorKings_DataBase.Domain.Aggregates")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MotorKings_DataBase/Models/MotorKings_DataBase.Models/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace MotorKings_DataBase.Models;
-
-public class Class1
-{
-}
